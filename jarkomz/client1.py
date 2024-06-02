005480e46950c6e7e74872692b313a5b38a82834
Luna <142798496+lilyabbae@users.noreply.github.com>
2024-06-02 12:31:33 +0700

    Update client1.py

--- jarkomz/client1.py
+++ jarkomz/client1.py
@@ -1,26 +1,20 @@
 import http.client
 
-def run_client():
-    ServerIP =input("masukan IP server:")
-    ServerPort=input("masukan Port server:")
-    conn = http.client.HTTPConnection(ServerIP, ServerPort)
+IP =input("Enter IP server: ")
+Port=input("Enter Port server: ")
+connection = http.client.HTTPConnection(IP, Port)
 
-    while True:
-        path = input("Enter path to request from server (or 'quit' to quit): ")
-        if path.lower() == 'quit':
-            break
+path = input("Enter path or leave blank to quit : ")
 
-        conn.request("GET", path)
+while path != ' ' :
+    connection.request("GET", path)
 
-        response = conn.getresponse()
+    response = connection.getresponse()
 
-        print("Response: ", response.status, response.reason)
-        print(response.headers)
+    print("Response: ", response.status, response.reason)
+    print(response.headers)
 
-        print(response.read().decode())
+    print(response.read().decode())
 
-    conn.close()
-    print("Connection to server closed")
-
-if __name__ == "__main__":
-    run_client()
+connconnection.close()
+print("Connection to server closed")
